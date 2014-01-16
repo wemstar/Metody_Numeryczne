255183a7431541d3484df6167c73731347b5bc60
Sylwester Macura <i1macura@stud104.(none)>
2014-01-16 17:09:39 +0100

    Laboratorium 16.01.2013

--- lab12/lab12.c
+++ lab12/lab12.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
-#include "../nr.h"
-#include "../nrutil.h"
-#include "../nrutil.c"
-#include "../../numerical_recipes.c/four1.c"
+#include "nr.h"
+#include "/home/labs/komputerowe_symulacje_numeryczne/numerical_recipes.c/nrutil.h"
+#include "/home/labs/komputerowe_symulacje_numeryczne/numerical_recipes.c/nrutil.c"
+#include "/home/labs/komputerowe_symulacje_numeryczne/numerical_recipes.c/four1.c"
 #define frand() ((double)rand())/(RAND_MAX+1.0)
 
 
@@ -14,19 +14,15 @@ float signal(float t,float T)
 	float omega=atan(1)*8.0/T;
 	return sin(omega*t)+sin(2*omega*t)+sin(3*omega*t);
 }
-float szum(float t,float T)
-{
-	return signal(t,T)+frand()-0.5;
-}
+float szum(float t,float T){return signal(t,T)+frand()-0.5;}
 float wagi(float t,float T)
 {
-	float sigma=t/20.0;
-	return 1/(sigma*sqrt(2*atan(1)*4))*exp(-pow(t,2.0)/(2*pow(sigma,2)));
-
+	float sigma=T/20.0;
+	float wynik=exp(-pow(t,2.0)/(2.0*pow(sigma,2)))/(sigma*sqrt(atan(1.0)*8.0));
+	return wynik;
 }
 void fourier(int k, float T,float tmax)
 {
-
 	float Nk=pow(2,k);
 	float dt=tmax/Nk;
 	float *f0=vector(1,2*Nk);
@@ -35,37 +31,51 @@ void fourier(int k, float T,float tmax)
 	float *g2=vector(1,2*Nk);
 
 	FILE* fp;
-	FILE* fp2;
 
-	if(k==8){fp=fopen("funkjce8.txt","w");fp2=fopen("transformata8.txt","w");}
-	if(k==10){fp=fopen("funkjce10.txt","w");fp2=fopen("transformata10.txt","w");}
-	if(k==12){fp=fopen("funkjce12.txt","w");fp2=fopen("transformata12.txt","w");}
+
+	if(k==8){fp=fopen("funkjce8.txt","w");}
+	if(k==10){fp=fopen("funkjce10.txt","w");}
+	if(k==12){fp=fopen("funkjce12.txt","w");}
 	
 	for(int i=1;i<=Nk;i++)
 	{
-		float t=i*dt;
+		float t=(i-1)*dt;
 		f0[2*i-1]=signal(t,T);
 		f0[2*i]=0.0;
 
 		f[2*i-1]=szum(t,T);
 		f[2*i]=0.0;
 
-		g1[2*i]=wagi(t,T);				
-		g1[2*i-2]=wagi(t,T);
+		g1[2*i-1]=wagi(t,T);
+				
+		g1[2*i]=0;
 
+		g2[2*i-1]=g1[2*i-1];
 		g2[2*i]=g1[2*i];
-		g2[2*i-2]=g1[2*i-2];
-
-		fprintf(fp, "%d %f %f\n",i,f0[2*i-1],f[2*i-1]);
 
 	}
 	four1(f,Nk,1);
-	for(int i=1;i<=2*Nk;++i)
+	four1(g1,Nk,1);
+	four1(g2,Nk,-1);
+	for(int i=1;i<=2*Nk;++i){g1[i]=g1[i]+g2[i];	}
+
+	for(int i=1;i<=Nk;++i)
 	{
-		fprintf(fp2, "%d %f\n",i,f[i]);
+		float a1=f[2*i-1];
+		float b1=f[2*i];
+		float a2=g1[2*i-1];
+		float b2=g1[2*i];
+		f[2*i-1]=a1*a2-b1*b2;
+		f[2*i]=a1*b2+a2*b1;
 	}
+
+	four1(f,Nk,-1);
+
+	float f_max=0;
+	for(int i=1;i<=Nk;++i)if(fabs(f[2*i-1])>f_max)f_max=fabs(f[2*i-1]);
+	for(int i=1;i<=Nk;++i)fprintf(fp, "%d %f %f\n",i,f0[2*i-1],f[2*i-1]*2.5/f_max );
+
 	fclose(fp);
-	fclose(fp2);
 	free_vector(f0,1,2*Nk);
 	free_vector(f,1,2*Nk);
 	free_vector(g1,1,2*Nk);
@@ -74,5 +84,7 @@ void fourier(int k, float T,float tmax)
 int main(int argc, char const *argv[])
 {
 	fourier(8, 1.0,3.0);
+	fourier(10, 1.0,3.0);
+	fourier(12, 1.0,3.0);
 	return 0;
 }
--- lab12/plot.p
+++ lab12/plot.p
@@ -2,13 +2,20 @@ set term jpeg
 set size 1,1
 
 
-set out 'n5.jpg'
+set out 'funkjce8.jpg'
 set xlabel 'x2'
 set ylabel 'epsylon'
 
-plot  'fourier1.txt' u 1:2 w l,'' u 1:3 w l
-set out 'four.jpg'
+plot  'funkjce8.txt' u 1:2 w l,'' u 1:3 w l
+
+set out 'funkjce10.jpg'
 set xlabel 'x2'
 set ylabel 'epsylon'
 
-plot 'transformataf.txt' u 1:2 w l
+plot  'funkjce10.txt' u 1:2 w l,'' u 1:3 w l
+
+set out 'funkjce12.jpg'
+set xlabel 'x2'
+set ylabel 'epsylon'
+
+plot  'funkjce12.txt' u 1:2 w l,'' u 1:3 w l
